7be41bd502f8252b6c7ec11cb709adb6abc96a6d
Habibullah <146019940+Habibullah-R@users.noreply.github.com>
2024-05-22 21:38:29 +0500

    Update RegisterAsShoutouter.js

--- js/RegisterAsShoutouter.js
+++ js/RegisterAsShoutouter.js
@@ -15,7 +15,6 @@ question_pc.forEach((qp, index)=>{
 // pop-up on hovering "?" sign for mobiles
 const question_m = document.querySelectorAll('.question-m')
 const question_m_div = document.querySelectorAll('.question-m-div')
-console.log(question_m_div)
 question_m.forEach((qm, index)=>{
     qm.addEventListener('click',()=>{
     question_m_div[index].style.display = 'flex';
@@ -24,3 +23,33 @@ question_m.forEach((qm, index)=>{
     }, 2000);
     })
 })
+
+// Country dropdown
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const selectDrop = document.querySelector('#countries');
+  
+    const fetchCitynames = async()=>{
+try{
+    const res = await fetch("https://api.first.org/data/v1/countries");
+    const resData = await res.json();
+    const data = resData.data;
+    console.log(resData)
+      let output = "";
+      Object.keys(data).map((count,index) => {
+        console.log(count)
+
+        output += `
+        
+        <option value="${count}">${count}</option>`;
+      })
+  
+      selectDrop.innerHTML = output;
+}catch(error){
+    console.log(error.message);
+}
+}
+fetchCitynames()
+  
+  });
