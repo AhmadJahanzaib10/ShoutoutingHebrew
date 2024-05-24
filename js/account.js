24b5d9be0ae5bfe90404f917139a43b33283a093
Habibullah <146019940+Habibullah-R@users.noreply.github.com>
2024-05-24 17:49:27 +0500

    Update account.js

--- js/account.js
+++ js/account.js
@@ -53,3 +53,48 @@ question_m.forEach((qm, index)=>{
     })
 })
 
+
+// Drag and drop
+const inputFile = document.getElementById('drag-drop');
+const myText = document.getElementById('text')
+const browse = document.getElementById('profile-pic')
+
+let file = "";
+
+browse.addEventListener('change',function (){
+    file = this.files[0];
+    imageFileURL();
+})
+
+inputFile.addEventListener('dragover',(ev)=>{
+    ev.preventDefault();
+    myText.textContent = "Release to upload"
+})
+
+inputFile.addEventListener('dragleave',(ev)=>{
+    myText.textContent = "drag and drop"
+})
+
+inputFile.addEventListener('drop',(ev)=>{
+    ev.preventDefault();
+    file = ev.dataTransfer.files[0];
+
+    imageFileURL();
+})
+
+const imageFileURL = ()=>{
+
+    let fileType = file.type;
+
+    let validateExtension = ['image/jpeg','image/png','image/jpg'];
+
+    if(validateExtension.includes(fileType)){
+        let fileReader = new FileReader();
+        fileReader.onload = ()=>{
+            let fileURL = fileReader.result;
+            console.log(fileURL)
+        }
+        fileReader.readAsDataURL(file)
+    }
+}
+
